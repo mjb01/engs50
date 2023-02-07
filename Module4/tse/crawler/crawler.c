2360607e9d679435f3e5a13b775b0e1a434d564d
Andy Trinh <andy.trinh.24@dartmouth.edu>
2023-02-07 13:54:12 -0500

    andycommit4

--- Module4/tse/crawler/crawler.c
+++ Module4/tse/crawler/crawler.c
@@ -17,59 +17,59 @@
 #include "hash.h"
 
 bool isInternalURL(const char *url, const char *base) {
-return (strstr(url, base) != NULL);
+    return (strstr(url, base) != NULL);
 }
 
 int main() {
-// 1. Create a single new webpage at depth 0, with the seed URL: https://thayer.github.io/engs50/
-webpage_t *page = webpage_new("https://thayer.github.io/engs50/", 0, NULL);
-if (page == NULL) {
-printf("Error: Failed to create webpage\n");
-exit(EXIT_FAILURE);
-}
+    // 1. Create a single new webpage at depth 0, with the seed URL: https://thayer.github.io/engs50/
+    webpage_t *page = webpage_new("https://thayer.github.io/engs50/", 0, NULL);
+    if (page == NULL) {
+        printf("Error: Failed to create webpage\n");
+        exit(EXIT_FAILURE);
+    }
 
-// 2. Fetch the webpage html to page->html
-if (webpage_fetch(page) != 0) {
-printf("Error: Failed to fetch webpage\n");
-exit(EXIT_FAILURE);
-}
+    // 2. Fetch the webpage html to page->html
+    if (webpage_fetch(page) != 0) {
+        printf("Error: Failed to fetch webpage\n");
+        exit(EXIT_FAILURE);
+    }
 
-// 3. Create an empty queue
-queue_t *q = qopen();
-if (q == NULL) {
-printf("Error: Failed to create queue\n");
-exit(EXIT_FAILURE);
-}
+    // 3. Create an empty queue
+    queue_t *q = qopen();
+    if (q == NULL) {
+        printf("Error: Failed to create queue\n");
+        exit(EXIT_FAILURE);
+    }
 
-// 4. Create an empty hash table
-hashtable_t *ht = hopen(1024);
-if (ht == NULL) {
-printf("Error: Failed to create hash table\n");
-exit(EXIT_FAILURE);
-}
+    // 4. Create an empty hash table
+    hashtable_t *ht = hopen(1024);
+    if (ht == NULL) {
+        printf("Error: Failed to create hash table\n");
+        exit(EXIT_FAILURE);
+    }
 
-// 5. Put the page into the queue and add the URL to the hash table
-if (qput(q, page) != 0) {
-printf("Error: Failed to put page into queue\n");
-exit(EXIT_FAILURE);
-}
+    // 5. Put the page into the queue and add the URL to the hash table
+    if (qput(q, page) != 0) {
+        printf("Error: Failed to put page into queue\n");
+        exit(EXIT_FAILURE);
+    }
 
-char *url = webpage_getURL(page);
-int len = strlen(url);
-if (hput(ht, page, url, len) != 0) {
-printf("Error: Failed to put URL into hash table\n");
-exit(EXIT_FAILURE);
-}
+    char *url = webpage_getURL(page);
+    int len = strlen(url);
+    if (hput(ht, page, url, len) != 0) {
+        printf("Error: Failed to put URL into hash table\n");
+        exit(EXIT_FAILURE);
+    }
 
-// 6. Iteratively process elements from the queue
-while (qsize(q) > 0) {
-// 7. Get the first page from the queue
-webpage_t *currPage = qget(q);
-if (currPage == NULL) {
-printf("Error: Failed to get page from queue\n");
-exit(EXIT_FAILURE);
-}
-// 8. Get the internal URLs from the current page's html
+    // 6. Iteratively process elements from the queue
+    while (qsize(q) > 0) {
+        // 7. Get the first page from the queue
+        webpage_t *currPage = qget(q);
+        if (currPage == NULL) {
+            printf("Error: Failed to get page from queue\n");
+            exit(EXIT_FAILURE);
+        }
+       // 8. Get the internal URLs from the current page's html
 int pos = 0;
 char *result;
 while ((result = webpage_getNextURL(currPage, &pos)) != NULL) {
@@ -91,16 +91,16 @@ exit(EXIT_FAILURE);
 
 // 12. Put the new page into the queue
 if (qput(q, newPage) != 0) {
-  printf("Error: Failed to put new page into queue\n");
-  exit(EXIT_FAILURE);
+printf("Error: Failed to put new page into queue\n");
+exit(EXIT_FAILURE);
 }
 
 // 13. Add the URL of the new page to the hash table
 char *newURL = webpage_getURL(newPage);
 int len = strlen(newURL);
 if (hput(ht, newPage, newURL, len) != 0) {
-  printf("Error: Failed to put new URL into hash table\n");
-  exit(EXIT_FAILURE);
+printf("Error: Failed to put new URL into hash table\n");
+exit(EXIT_FAILURE);
 }
 }
 }
